33ba024cd9a63509d7cf5ada75c27bd712472638
av6developer <av6developer@gmail.com>
2026-03-27 13:35:00 +0530

    small chnage in the server in the gateway -- Animesh Added pharmacy

--- apps/gateway/src/server.ts
+++ apps/gateway/src/server.ts
@@ -6,7 +6,8 @@ import { IS_REDIS, PORT } from "@repo/shared";
 import { initializeCache as initializeCoreCache } from "@apps/core/config/redisClient.js";
 import { initializeCache as initializeOpdCache } from "@apps/opd/config/redisClient.js";
 import { createOpdApp } from "@apps/opd";
-
+import { createPharmacyApp } from "@apps/pharmacy";
+import { initializeCache as initializePharmacyCache } from "@apps/pharmacy/config/redisClient.js";
 const app = express();
 
 const enabled = new Set(
@@ -15,6 +16,8 @@ const enabled = new Set(
 
 if (enabled.has("core")) app.use("/api/v1/core", createCoreApp("GATEWAY"));
 if (enabled.has("opd")) app.use("/api/v1/opd", createOpdApp("GATEWAY"));
+if (enabled.has("pharmacy"))
+  app.use("/api/v1/pharmacy", createPharmacyApp("GATEWAY"));
 
 connectRedis()
   .then(() => {
@@ -23,6 +26,7 @@ connectRedis()
       console.log(`gateway running on ${PORT}`);
       if (IS_REDIS && enabled.has("core")) await initializeCoreCache();
       if (IS_REDIS && enabled.has("opd")) await initializeOpdCache();
+      if (IS_REDIS && enabled.has("pharmacy")) await initializePharmacyCache();
     });
   })
   .catch((err) => {
