8c88fb89ac7e8ad14720489453d779eeeceff486
Samrat0709 <118931358+Samrat0709@users.noreply.github.com>
2026-04-08 12:57:14 +0530

    update short code name in excelMapping

--- apps/pharmacy/src/mapper/excelMapping.ts
+++ apps/pharmacy/src/mapper/excelMapping.ts
@@ -1,5 +1,5 @@
 import { DistributorResponse } from "@/types/distributor/distributor.js";
-import { SHORT_CODE } from "@repo/shared/utils/shortCode/pharmacy.shortCode.utils.js";
+import { SHORT_CODE as PMS_SHORT_CODE } from "@repo/shared/utils/shortCode/pharmacy.shortCode.utils.js";
 import { createDistributorServiceValidation } from "@/validations/service/distributor/distributor.service.validation.js";
 import { City, State } from "@repo/db/generated/prisma/client";
 import {
@@ -18,13 +18,14 @@ import {
 } from "@apps/core/mapper/master/city.mapper.js";
 import { createCityServiceValidation } from "@apps/core/validations/service/master/city.service.validation.js";
 import { nameStateServiceValidation } from "@apps/core/validations/service/master/state.service.validation.js";
+import { SHORT_CODE } from "@repo/shared/utils/shortCode/core.shortCode.utils.js";
 
 export const mappingExport: {
   [key: string]: (record: unknown) => unknown;
 } = {
   [SHORT_CODE.STATE]: (record: unknown) => toExcelFromState(record as State),
   [SHORT_CODE.CITY]: (record: unknown) => toExcelFromCity(record as City),
-  [SHORT_CODE.DISTRIBUTOR]: (record: unknown) =>
+  [PMS_SHORT_CODE.DISTRIBUTOR]: (record: unknown) =>
     toDistributorExcel(record as DistributorResponse),
 };
 
@@ -52,7 +53,7 @@ export const mappingImport: {
       validation: createCountryServiceValidation,
     };
   },
-  [SHORT_CODE.DISTRIBUTOR]: () => {
+  [PMS_SHORT_CODE.DISTRIBUTOR]: () => {
     return {
       mapper: fromDistributorExcel,
       validation: createDistributorServiceValidation,
