96c38eae6d7e3aaddac81b90860367d0ddde49f9
Samrat0709 <118931358+Samrat0709@users.noreply.github.com>
2026-06-06 12:03:27 +0530

    small chnage in settings validation -- Samrat

--- apps/inventory/src/validations/request/master/settings.validation.ts
+++ apps/inventory/src/validations/request/master/settings.validation.ts
@@ -1,35 +1,43 @@
 import { CreateOrUpdateSettings } from "@/types/master/settings.js";
 import {
+  CalculationMethod,
+  ItemStockType,
+  RoundFormat,
+} from "@repo/db/generated/prisma/enums.js";
+import {
   boolRequired,
   enumOptional,
-  idOptional,
-  idRequired,
+  intOptional,
+  intRequired,
   strOptional,
 } from "@repo/shared/utils/joi.utils.js";
 import { validationHandler } from "@repo/shared/utils/requestValidationHelper.js";
-import {
-  CalculationMethod,
-  RoundFormat,
-} from "@repo/db/generated/prisma/client";
 import Joi from "joi";
 
 export const createOrUpdateSettingsSchema = Joi.object<CreateOrUpdateSettings>({
   isEmail: boolRequired("Is Email"),
   isSMS: boolRequired("Is SMS"),
+  isAccounting: boolRequired("Is Accounting"),
   warehouseMode: boolRequired("Warehouse Mode"),
   supplierMode: boolRequired("Supplier Mode"),
   isWhatsapp: boolRequired("Is Whatsapp"),
-  expiryInMonth: idRequired("Expiry in month"),
+  expiryInMonth: intRequired("Expiry in month"),
   countryCode: strOptional("Country code"),
-  batchSize: idOptional("Batch size"),
+  batchSize: intOptional("Batch size"),
 
-  defaultPrecision: idRequired("Default precision"),
+  defaultPrecision: intOptional("Default precision", 0),
   grnCalculationMethod: enumOptional("GRN calculation", CalculationMethod),
 
   grnRoundedFormat: enumOptional("GRN rounded format", RoundFormat),
 
   grnFinalRoundedFormat: enumOptional("GRN final rounded format", RoundFormat),
-  isAccounting: boolRequired("Is Accounting"),
+
+  grnPrecision: intOptional("GRN precision", 0),
+  poPrecision: intOptional("PO precision", 0),
+  itemPrecision: intOptional("Item precision", 0),
+  poCalculationMethod: enumOptional("PO calculation method", CalculationMethod),
+  itemStockType: enumOptional("Item stock type", ItemStockType),
+  poRoundedFormat: enumOptional("PO rounded format", RoundFormat),
 });
 
 export const validateSettings = validationHandler({
