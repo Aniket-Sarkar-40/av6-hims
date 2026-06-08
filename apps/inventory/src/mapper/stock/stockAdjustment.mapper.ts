aec851fe0b0e78038b94233e007a995ce47ad4d3
Samrat0709 <118931358+Samrat0709@users.noreply.github.com>
2026-06-08 13:24:24 +0530

    small chnage in the mapper -- Samrat

--- apps/inventory/src/mapper/stock/stockAdjustment.mapper.ts
+++ apps/inventory/src/mapper/stock/stockAdjustment.mapper.ts
@@ -1,3 +1,4 @@
+import { commonService } from "@/services/common.service.js";
 import { itemMasterService } from "@/services/master/itemMaster.service.js";
 import {
   StockAdjustmentDetailsDTO,
@@ -8,8 +9,24 @@ import { InvStockAdjustmentDetails } from "@repo/db/generated/prisma/client";
 import { customOmit, toIdValue } from "av6-utils";
 
 export const toStockAdjustmentDTO = async (
-  data: StockAdjustmentResponse[],
+  data: StockAdjustmentResponse[]
 ): Promise<StockAdjustmentDTO[]> => {
+  const allBranches = await commonService.getAllElements<"InvBranch">({
+    cacheCode: "BRANCH",
+    canNullReturnable: true,
+    modelName: "InvBranch",
+    shortCode: "BRANCH",
+    useActiveFlag: true,
+  });
+
+  const allWarehouses = await commonService.getAllElements<"InvWarehouse">({
+    cacheCode: "WAREHOUSE",
+    canNullReturnable: true,
+    modelName: "InvWarehouse",
+    shortCode: "WAREHOUSE",
+    useActiveFlag: true,
+  });
+
   return Promise.all(
     data.map(async (stockAdjustment) => {
       const omittedStockAdjustment = customOmit<
@@ -57,22 +74,31 @@ export const toStockAdjustmentDTO = async (
           ]);
           const item = await itemMasterService.getItemMasterByIdWoDto(
             detail.itemId,
-            true,
+            true
           );
 
           return {
             ...omittedDetail.rest,
             item,
           };
-        }),
+        })
       );
+
+      const cc =
+        allWarehouses.find((c) => c.id === stockAdjustment.ccId) ??
+        allBranches.find((c) => c.id === stockAdjustment.ccId) ??
+        null;
+      const targetCc =
+        allWarehouses.find((c) => c.id === stockAdjustment.targetCcId) ??
+        allBranches.find((c) => c.id === stockAdjustment.targetCcId) ??
+        null;
 
       return {
         ...omittedStockAdjustment.rest,
-        collectionCenter: toIdValue(stockAdjustment.cc, "colName"),
-        targetCollectionCenter: toIdValue(stockAdjustment.targetCc, "colName"),
+        collectionCenter: toIdValue(cc, "name"),
+        targetCollectionCenter: toIdValue(targetCc, "name"),
         stockAdjustmentDetails: detailDTO,
       };
-    }),
+    })
   );
 };
